6be3767644bc71d309dfcf730cad1367e55e1e9a
Max2 <chigov@gmail.com>
2022-06-08 18:41:01 +0300

    sales060622

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  //create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Mike","Stoba",'chigov@gmail.com',5000),
+    new SalesPerson("Alex","Gerasimluik",'alex@gmail.com',6000),
+    new SalesPerson("Max","Stoba",'max@gmail.com',7000),
+    new SalesPerson("Alex","Aganasiev",'afa11afa@gmail.com',8000),
+  ];
+
 
   constructor() { }
 
